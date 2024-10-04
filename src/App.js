fe6c1a226236e374ed0a4cee3ec909ae28163ded
codeHysteria28 <graphicglobal67@gmail.com>
2024-10-04 16:49:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react'
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { FaRobot } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
+
 import './App.css';
 
 function App() {
+  const lastMessageRef = useRef();
+  const [aiMessage, setAiMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = () => {
+    setMessages(prevMessages => [...prevMessages, { sender: 'user', text: aiMessage }]);
+    setAiMessage("");
+
+    axios({
+        method: 'post',
+        url: 'http://localhost:7071/api/enhanceText',
+        data: {aiMessage},
+        headers: { 'Content-Type': 'application/json' }
+    }).then(res => {
+        setMessages(prevMessages => [...prevMessages, { sender: 'ai', text: res.data }]);
+    });
+}
+
+  useEffect(() => {
+    if(lastMessageRef.current){
+      lastMessageRef.current.scrollIntoView({ behaviour: 'smooth' });
+    }
+  }, [messages])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='aiInterfaceVisible'>
+          <div className="chat">
+              <div className="assistaneChatMessage">
+                  <FaRobot className="messageAiIcon"/>
+                  Hello I'm your assistant. How can I help you today ?
+              </div>
+              {/* {
+                  messages.map((message, index) => {
+                      return (
+                          <div key={index} ref={index === messages.length -1 ? lastMessageRef : null} className={message.sender === 'user' ? 'chatMessage' : 'assistaneChatMessage'}>
+                              {message.sender === 'user' ? <FaUser className="messageAiIcon"/> : <FaRobot className="messageAiIcon"/>}
+                              {message.text}
+                          </div>
+                      )
+                  })
+              } */}
+          </div>
+          <div className="userPromtArea">
+              <textarea 
+                  className="userInput" 
+                  value={aiMessage} 
+                  placeholder="Type your message here..." 
+                  onChange={e => setAiMessage(e.target.value)} 
+                  onKeyDown={e => { if (e.key === "Enter" && aiMessage.trim() !== ""){ e.preventDefault(); sendMessage(); } }}></textarea>
+              <Button className="primary sendChat" onClick={sendMessage}><IoSend className="sendIcon"/></Button>
+          </div>
+      </div>
   );
 }
 
